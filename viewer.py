c75cefc5a0420fecaefcfd88e1b2da6904098728
markokanada <39034479+markokanada@users.noreply.github.com>
2023-12-26 15:42:28 +0100

    refresh and close init.

--- viewer.py
+++ viewer.py
@@ -28,12 +28,28 @@ class Viewer(ABC):
         Implementation is in progress.
         """
         pass
-    
+
     @abstractmethod
     def ChampionSelector(self, championID):
         """
         Its a special UILoader, because this function require a championID which is the selcted champion which we want to grow,
         and with this information this function will set the enviromental informations, and load the graphics.
+        Implementation is in progress.
+        """
+        pass
+
+    @abstractmethod
+    def refresh(self):
+        """
+        Refresh the contents of the view.
+        Implementation is in progress.
+        """
+        pass
+
+    @abstractmethod
+    def close(self):
+        """
+        Close the viewer.
         Implementation is in progress.
         """
         pass
