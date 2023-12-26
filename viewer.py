de69200ff5c90fc609033fc4bb0d56d9906fb101
markokanada <39034479+markokanada@users.noreply.github.com>
2023-12-26 15:40:46 +0100

    UILoader init.

--- viewer.py
+++ viewer.py
@@ -4,3 +4,12 @@ class Viewer(ABC):
     def __init__(self, title):
         self.title = title
         super().__init__()
+        
+    @abstractmethod
+    def UILoader(self, sceneID):
+        """
+        Load the different UI scenes by the given argument, which is an ID of a static collection.
+        Implementation is in progress.
+        """
+        pass
+
