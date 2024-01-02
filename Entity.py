93d2a06e8a3c8bf63775cfebd72aa1079cd23ee5
csokamarton <csoka.marton2005@gmail.com>
2024-01-02 17:47:02 +0100

    Voice properties added

--- Entity.py
+++ Entity.py
@@ -1,7 +1,9 @@
 from PIL import Image
 
 class Entity:
-    def __init__(self, deadImgPath, normalImgPath, hungryImgPath, thirstyImgPath):
+    def __init__(self, deadImgPath, 
+                 normalImgPath, hungryImgPath, 
+                 thirstyImgPath, ):
         self.DeadImg = Image.open(deadImgPath)
         self.NormalImg = Image.open(normalImgPath)
         self.HungryImg = Image.open(hungryImgPath)
