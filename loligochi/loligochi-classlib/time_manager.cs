3794c4cbe898604a1fca04cf80dacf13c65458e3
molmardo <molnarmark2005@gmail.com>
2024-01-03 13:55:28 +0100

    time manager create

---
+++ loligochi/loligochi-classlib/time_manager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loligochi_classlib
+{
+    internal class time_manager
+    {
+        private DateTime StartTime { get; set; }
+        private DateTime LastCheck { get; set; }
+
+        public time_manager()
+        {
+            StartTime = DateTime.Now;
+            LastCheck = StartTime;
+        }
+
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+
+        // Returns the elapsed time since the last check.
+        public TimeSpan GetElapsedTime()
+        {
+            DateTime now = GetCurrentTime();
+            TimeSpan elapsed = now - LastCheck;
+            LastCheck = now;
+            return elapsed;
+        }
+
+        // Returns the total elapsed time since the start.
+        public TimeSpan GetTotalElapsedTime()
+        {
+            return DateTime.Now - StartTime;
+        }
+    }
+}
