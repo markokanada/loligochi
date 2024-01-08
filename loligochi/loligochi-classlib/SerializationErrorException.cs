90453885344989319630981be9d106609f24162f
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-08 21:43:35 +0100

    SerializationErrorException added

---
+++ loligochi/loligochi-classlib/SerializationErrorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loligochi_classlib
+{
+    public class SerializationErrorException : Exception
+    {
+        public SerializationErrorException():base ("Error Code 3: There was an error in the process of serialization which could happened because of file corruption or data loss. The game cant start with this state. ")
+        {
+            
+        }
+    }
+}
