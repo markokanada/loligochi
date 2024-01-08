35b84e910d19fa041d61f651f51fa1990ece8e6c
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-08 21:33:56 +0100

    FileMissingException added

---
+++ loligochi/loligochi-classlib/FileMissingException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loligochi_classlib
+{
+    public class FileMissingException : Exception
+    {
+        public FileMissingException() : base("Error code 2: The given FileSource was null. A file is could be corrupt or missing! ")
+        {
+            
+        }
+    }
+}
