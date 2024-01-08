c26041fa6cd03862ad2ceeec4d1015e02d1290e0
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-08 23:57:42 +0100

    Save loading & Selection V1 - WIP

--- loligochi/loligochi-app/MainWindow.xaml.cs
+++ loligochi/loligochi-app/MainWindow.xaml.cs
@@ -33,6 +33,8 @@ namespace loligochi_app
         private int champ_index { get; set; } = 2;
         private static string[] allChampionJsonFiles = Directory.GetFiles("src/jsons/default-champion-datas/", "*.json").OrderBy(name => name).ToArray();
         private Entity? champ { get; set; } = null;
+
+        private int save_index { get; set; } = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -107,7 +109,9 @@ namespace loligochi_app
         }
         private void Continue_Game_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Init_Save_Slots();
+            Main_Menu_Scene.Visibility = Visibility.Hidden;
+            Load_Game_Scene.Visibility = Visibility.Visible;
         }
         private void Settings_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -250,8 +254,192 @@ namespace loligochi_app
 
         #endregion
 
-        #region Other function & event handlers which used at more places.
+        #region In_Game_Scene
 
+        #endregion
+
+        #region Load_Saved_Game
+
+
+        private void Back_To_The_Main_Menu_From_Load_Game_Scene(object sender, RoutedEventArgs e)
+        {
+            Load_Game_Scene.Visibility = Visibility.Hidden;
+            Main_Menu_Scene.Visibility = Visibility.Visible;
+        }
+
+        private void Save_Select_Option_1_Clicked (object sender, RoutedEventArgs e)
+        {
+            if (Save_Select_Scene_Option_1.IsEnabled)
+            {
+                Load_Save_From_Save_Select(Save_Select_Scene_Option_1.Text);
+            }
+        }
+        private void Save_Select_Option_2_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (Save_Select_Scene_Option_2.IsEnabled)
+            {
+                Load_Save_From_Save_Select(Save_Select_Scene_Option_1.Text);
+            }
+        }
+        private void Save_Select_Option_3_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (Save_Select_Scene_Option_3.IsEnabled)
+            {
+                Load_Save_From_Save_Select(Save_Select_Scene_Option_1.Text);
+            }
+        }
+        private void Save_Select_Option_4_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (Save_Select_Scene_Option_4.IsEnabled)
+            {
+                Load_Save_From_Save_Select(Save_Select_Scene_Option_1.Text);
+            }
+        }
+
+        private void Rotate_The_Save_Slots_By_Arrow_Down(object sender, RoutedEventArgs e)
+        {
+            List <String> avaibleSaves = new List<String>();
+
+            if (!Directory.Exists("src"))
+            {
+                throw new FileMissingException();
+            }
+            else if (!Directory.Exists("src/save"))
+            {
+                avaibleSaves = [];
+
+            }
+            else
+            {
+                avaibleSaves = Directory.GetFiles("src/save").ToList();
+            }
+
+            if(avaibleSaves.Count() < save_index+1) //Több az index, mint a save -> már csak az üres találatokat pörgeti
+            {
+                if (Save_Select_Scene_Option_4.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_4.Text = "-Empty Save Slot-";
+                }
+                else if (Save_Select_Scene_Option_3.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_3.Text = "-Empty Save Slot-";
+                }
+                else if (Save_Select_Scene_Option_2.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_2.Text = "-Empty Save Slot-";
+                }
+                else if (Save_Select_Scene_Option_1.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_1.Text = "-Empty Save Slot-";
+                }
+                save_index++;
+            }
+            else
+            {
+                Save_Select_Scene_Option_1.Text = Save_Select_Scene_Option_2.Text;
+                Save_Select_Scene_Option_2.Text = Save_Select_Scene_Option_3.Text;
+                Save_Select_Scene_Option_3.Text = Save_Select_Scene_Option_4.Text;
+                Save_Select_Scene_Option_4.Text = avaibleSaves[save_index];
+
+                save_index++;
+            }
+        }
+
+        private void Rotate_The_Save_Slots_By_Arrow_Up(object sender, RoutedEventArgs e)
+        {
+            List<String> avaibleSaves = new List<String>();
+
+            if (!Directory.Exists("src"))
+            {
+                throw new FileMissingException();
+            }
+            else if (!Directory.Exists("src/save"))
+            {
+                avaibleSaves = [];
+
+            }
+            else
+            {
+                avaibleSaves = Directory.GetFiles("src/save").ToList();
+            }
+
+            if(save_index != 0) { 
+            if (avaibleSaves.Count() < save_index + 1)
+            {
+                if (Save_Select_Scene_Option_4.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_4.Text = "-Empty Save Slot-";
+                }
+                else if (Save_Select_Scene_Option_3.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_3.Text = "-Empty Save Slot-";
+                }
+                else if (Save_Select_Scene_Option_2.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_2.Text = "-Empty Save Slot-";
+                }
+                else if (Save_Select_Scene_Option_1.Text != "-Empty Save Slot-")
+                {
+                    Save_Select_Scene_Option_1.Text = "-Empty Save Slot-";
+                }
+                save_index--;
+            }
+            else
+            {
+                Save_Select_Scene_Option_4.Text = Save_Select_Scene_Option_1.Text;
+                Save_Select_Scene_Option_3.Text = Save_Select_Scene_Option_2.Text;
+                Save_Select_Scene_Option_2.Text = Save_Select_Scene_Option_1.Text;
+                Save_Select_Scene_Option_1.Text = avaibleSaves[save_index];
+
+                save_index--;
+            }
+            }
+        }
+
+        private void Init_Save_Slots()
+        {
+            List<String> avaibleSaves = new List<String>();
+
+            if (!Directory.Exists("src"))
+            {
+                throw new FileMissingException();
+            }
+            else if (!Directory.Exists("src/save"))
+            {
+                avaibleSaves = [];
+
+            }
+            else
+            {
+                avaibleSaves = Directory.GetFiles("src/save").ToList();
+            }
+            if (avaibleSaves.Count >= 4)
+            {
+                Save_Select_Scene_Option_4.Text = avaibleSaves[3];
+            }
+            if (avaibleSaves.Count >= 3)
+            {
+                Save_Select_Scene_Option_3.Text = avaibleSaves[2];
+            }
+            if (avaibleSaves.Count >= 2)
+            {
+                Save_Select_Scene_Option_2.Text = avaibleSaves[1];
+            }
+            if (avaibleSaves.Count >= 1)
+            {
+                Save_Select_Scene_Option_1.Text = avaibleSaves[0];
+            }
+            save_index += 4;
+        }
+
+
+        #endregion
+
+        #region Other function & event handlers which used at more places.
+        private void Load_Save_From_Save_Select(string saveName)
+        {
+
+        }
         private void Load_Champ_In_Champ_Select_Preview()
         {
             champ = Envirovment.DeserializeEntity(allChampionJsonFiles[champ_index]);
