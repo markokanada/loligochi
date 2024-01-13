dab876e2873f4a7829888093a6bcf061c1d74d5a
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-13 01:03:48 +0100

    Create envirovmentIsNullException

---
+++ loligochi/loligochi-classlib/EnvirovmentIsNullException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loligochi_classlib
+{
+    public class EnvirovmentIsNullException: Exception
+    {
+        public EnvirovmentIsNullException() : base("Error code 5: The given Envirovment was null.")
+        { }
+    }
+}
