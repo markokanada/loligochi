1aceb84be73635f10f5a08d22981c442f4b6aad9
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-14 21:59:37 +0100

    Test SerializeEntity_CreatesFileWithProperContent

--- loligochi/loligochi-testlib/EnvirovmentTests.cs
+++ loligochi/loligochi-testlib/EnvirovmentTests.cs
@@ -9,7 +9,33 @@ namespace loligochi_tests
     [TestClass]
     public class EnvirovmentTests
     {
-        
-       
+        [TestMethod]
+        public void SerializeEntity_CreatesFileWithProperContent()
+        {
+            // Arrange
+            var entity = new Entity("deadImage", "normalImage", "hungryImage",
+                "thirstyImage", "angryImage", "sickImage",
+                "deadVoice", "normalVoice", "sickVoice",
+                "hungryVoice", "thirstyVoice", "angryVoice",
+                "Normal", "TestEntity", "BasedOnEntity", 1,
+                5, 100, 50, 50, false, 0, 100, 50, DateTime.Now);
+
+            var envirovment = new Envirovment();
+            var fileName = "TestEntity";
+
+            Directory.CreateDirectory("src/save");
+
+            var result = Envirovment.SerializeEntity(entity, fileName);
+            var filePath = Path.Combine("src/save", fileName + ".json");
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(File.Exists(filePath));
+            var jsonString = File.ReadAllText(filePath);
+            var deserializedEntity = JsonSerializer.Deserialize<Entity>(jsonString);
+            Assert.IsNotNull(deserializedEntity);
+            Assert.AreEqual("TestEntity", deserializedEntity.Name);
+
+            File.Delete(filePath);
+        }
     }
 }
