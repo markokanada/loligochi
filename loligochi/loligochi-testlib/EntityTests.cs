6d2fd3214dcf5379b2d6ec820ba15f8841aae455
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-14 19:50:30 +0100

    Test EntitySicknessLevel_CannotBeSetBelowZero()

---
+++ loligochi/loligochi-testlib/EntityTests.cs
@@ -0,0 +1,47 @@
+namespace loligochi_testlib
+{
+    [TestClass]
+    public class EntityTests
+    {
+        private Entity CreateTestEntity(DateTime? lastSaw = null)
+        {
+            return new Entity(
+                deadImage: "deadImage",
+                normalImage: "normalImage",
+                hungryImage: "hungryImage",
+                thirstyImage: "thirstyImage",
+                angryImage: "angryImage",
+                sickImage: "sickImage",
+                deadVoice: "deadVoice",
+                normalVoice: "normalVoice",
+                sickVoice: "sickVoice",
+                hungryVoice: "hungryVoice",
+                thirstyVoice: "thirstyVoice",
+                angryVoice: "angryVoice",
+                currentStatus: "Normal",
+                name: "TestEntity",
+                basedOn: "BasedOnEntity",
+                level: 1,
+                age: 5,
+                hp: 50,
+                hungerLevel: 50,
+                thirstLevel: 50,
+                isTheEntitySick: false,
+                entitySicknessLevel: 0,
+                maximumHP: 100,
+                baseHP: 50,
+                lastSaw: lastSaw
+            );
+        }
+        [TestMethod]
+        public void EntitySicknessLevel_CannotBeSetBelowZero()
+        {
+            Entity entity = CreateTestEntity();
+            double invalidValue = -1;
+
+            entity.EntitySicknessLevel = invalidValue;
+
+            Assert.AreEqual(0, entity.EntitySicknessLevel);
+        }
+    }
+}
