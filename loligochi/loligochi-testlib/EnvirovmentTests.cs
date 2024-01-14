e73f331a0fc6060b1405213f44b130acd2a06fc5
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-14 21:56:55 +0100

    EnvirovmentTests init

---
+++ loligochi/loligochi-testlib/EnvirovmentTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using loligochi_classlib;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace loligochi_tests
+{
+    [TestClass]
+    public class EnvirovmentTests
+    {
+        
+       
+    }
+}
