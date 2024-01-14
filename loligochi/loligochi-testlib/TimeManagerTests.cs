fc3959578ae6d98b6df1af6211fa6d1561bf986e
markokanada <39034479+markokanada@users.noreply.github.com>
2024-01-14 21:37:46 +0100

    Test TimeManager_Constructor_SetsStartTimeToCurrentTime

---
+++ loligochi/loligochi-testlib/TimeManagerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loligochi_testlib
+{
+    [TestClass]
+    public class TimeManagerTests
+    {
+        [TestMethod]
+        public void TimeManager_Constructor_SetsStartTimeToCurrentTime()
+        {
+            var beforeCreation = DateTime.Now;
+
+            var timeManager = new TimeManager();
+            var afterCreation = DateTime.Now;
+
+            Assert.IsTrue(timeManager.StartTime >= beforeCreation && timeManager.StartTime <= afterCreation);
+        }
+    }
+}
